b42801f45d8493030a3fbb54b22165594d288ca1
Pixel <pixelpanda0119@gmail.com>
2025-09-17 20:50:03 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
-pip install --upgrade pip setuptools
-pip install -r requirements.txt
 apt-get update && apt-get install -y libgl1
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
