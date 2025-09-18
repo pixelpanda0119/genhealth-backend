2b8ca5f3c39043be2bc9429bd06d4a9ad76c261b
Pixel <pixelpanda0119@gmail.com>
2025-09-17 21:12:01 -0400

    Add docker file

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-export PATH="$HOME/.cargo/bin:$PATH"
-apt-get update && apt-get install -y libgl1
-pip install --upgrade pip setuptools
-pip install -r requirements.txt
